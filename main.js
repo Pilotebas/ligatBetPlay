e5f32acc66df7822ea30334de010d3cfb4749550
Pilotebas <149727792+Pilotebas@users.noreply.github.com>
2024-07-19 16:22:48 +0000

    feat: :sparkles: Se sube primer avanze a la posible conexion

---
+++ main.js
@@ -0,0 +1,13 @@
+import { actividad } from "./js/modules/actividad";
+
+let conexion= {
+    user: hola,
+    port: Juan,
+    pass: 1023,
+    host: 123,
+    cluster: serveo,
+    dbName: hola
+}
+let hola = new actividad(conexion)
+
+console.log(hola);
