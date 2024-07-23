d87a9094c0d122e23dbf0b7422b2b40b66ea8f81
Juan Sebastián Rivas Uribe <156709972+JuanRivasfr@users.noreply.github.com>
2024-07-23 20:05:35 +0000

    feat: :sparkles: Finalizacion del caso de uso numero 3

--- main.js
+++ main.js
@@ -3,4 +3,7 @@ import {comunicacion} from "./js/modules/comunicacion.js";
 
 let doy = new comunicacion()
 
-console.log(await doy.getAllMatch());
+//console.log(await doy.getAllMatch());
+//console.log(await doy.objetoc3());
+//console.log(await doy.eliminarPartido());
+//console.log(await doy.editarPartido());
