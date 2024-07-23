431e7319c3762ff635c22278c080ff93dd44a2f7
Juan Sebastián Rivas Uribe <156709972+JuanRivasfr@users.noreply.github.com>
2024-07-23 21:05:01 +0000

    feat: :sparkles: Finalizacion del caso de uso 7

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 
-import {comunicacion} from "./js/modules/comunicacion.js";
+import {partido} from "./js/modules/partido.js";
+import {equipo} from "./js/modules/equipo.js";
+import { jugador } from "./js/modules/jugador.js";
+import { entrenador } from "./js/modules/entrenador.js";
 
-let doy = new comunicacion()
-
-//console.log(await doy.getAllMatch());
+let doy = new entrenador()
+//Caso 3
 //console.log(await doy.objetoc3());
 //console.log(await doy.eliminarPartido());
 //console.log(await doy.editarPartido());
+
+//Caso 7
+//console.log(await doy.objetoc7());
+//console.log(await doy.eliminarEntrenador()); 
+//console.log(await doy.editarEntrenador());
